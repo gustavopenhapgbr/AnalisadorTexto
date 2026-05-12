1b479de716f49baf9baf428b1470a635502dc5fa
gustapgbr <gustavodossantospgbr@gmail.com>
2026-05-12 02:24:09 -0300

    Criando a Função contar vogais

--- src/AnalisadorTexto.java
+++ src/AnalisadorTexto.java
@@ -19,4 +19,20 @@ public class AnalisadorTexto {
         return palavras.length;
     }
 
+
+    static int contarVogais(String texto) {
+        int contador = 0;
+        char[] letras = texto.toLowerCase().toCharArray();
+
+        for (int i = 0; i < letras.length; i++) {
+            char c = letras[i];
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                    || c == 'á' || c == 'é' || c == 'í' || c == 'ó' || c == 'ú'
+                    || c == 'â' || c == 'ê' || c == 'î' || c == 'ô' || c == 'û'
+                    || c == 'ã' || c == 'õ' || c == 'à') {
+                contador++;
+            }
+        }
+        return contador;
+    }
 }
